57612ee6988076941d93f11a088266e2c00772a9
luxxi <luka.domitrovic@livestorm.co>
2025-03-28 10:32:14 +0100

    linter

--- src/app/controllers/datasets_controller.rb
+++ src/app/controllers/datasets_controller.rb
@@ -1,11 +1,11 @@
 class DatasetsController < ApplicationController
-  def index    
+  def index
     @search = Dataset.search do
       if params[:search].present?
-        search_term = params[:search].split.map { |term| 
+        search_term = params[:search].split.map { |term|
           term = Solr::Escape.escape(term.downcase) + "*"
         }.join(" AND ")
-        
+
         fulltext search_term  do
           fields(
             text_search: 2.0,
@@ -13,15 +13,15 @@ class DatasetsController < ApplicationController
           )
         end
       end
-      
+
       adjust_solr_params do |params|
         facet_fields = %w[
-          organisms_sm 
-          cell_types_sm 
-          tissues_sm 
-          developmental_stages_sm 
-          diseases_sm 
-          sexes_sm 
+          organisms_sm
+          cell_types_sm
+          tissues_sm
+          developmental_stages_sm
+          diseases_sm
+          sexes_sm
           technologies_sm
           source_name_sm
         ]
@@ -59,7 +59,7 @@ class DatasetsController < ApplicationController
 
         params[:"facet.limit"] = -1
       end
-      
+
       facet :organisms, sort: :count
       facet :cell_types, sort: :index
       facet :tissues, sort: :index
@@ -70,7 +70,7 @@ class DatasetsController < ApplicationController
       facet :source_name, sort: :index
 
       with(:organism_ancestors, params[:organisms]) if params[:organisms].present?
-      
+
       with(:sexes, params[:sex]) if params[:sex].present?
       with(:cell_types, params[:cell_types]) if params[:cell_types].present?
       with(:tissues, params[:tissues]) if params[:tissues].present?
@@ -78,9 +78,9 @@ class DatasetsController < ApplicationController
       with(:diseases, params[:diseases]) if params[:diseases].present?
       with(:technologies, params[:technologies]) if params[:technologies].present?
       with(:source_name, params[:source_name]) if params[:source_name].present?
-      
+
       paginate page: params[:page] || 1, per_page: 6
-      
+
       data_accessor_for(Dataset).include = [
         :sexes,
         :cell_types,
@@ -94,10 +94,10 @@ class DatasetsController < ApplicationController
         :links
       ]
     end
-    
+
     @datasets = @search.results
     @organism_facet_rows = OrganismFacetBuilder.build_from_facets(@search) || []
-    
+
     respond_to do |format|
       format.html
       format.turbo_stream
--- src/lib/tasks/ontology_terms.rake
+++ src/lib/tasks/ontology_terms.rake
@@ -2,16 +2,16 @@ namespace :ontology_terms do
   desc "Find and update missing ontology_term_id based on tax_id"
   task update_for_organisms: :environment do
     organisms_to_update = Organism.where(ontology_term_id: nil).where.not(tax_id: nil)
-    
+
     puts "Found #{organisms_to_update.count} organisms with missing ontology_term_id"
-  
+
     updated_count = 0
     not_found_count = 0
-      
+
     organisms_to_update.find_each do |organism|
       ncbi_taxon_identifier = "NCBITaxon:#{organism.tax_id}"
       ontology_term = OntologyTerm.find_by(identifier: ncbi_taxon_identifier)
-      
+
       if ontology_term
         organism.update(ontology_term_id: ontology_term.id)
         updated_count += 1
