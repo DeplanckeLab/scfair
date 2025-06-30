8c4f1fab8778cf6b333548d318a742e310801809
luxxi <luka.domitrovic@livestorm.co>
2025-06-30 21:25:31 +0200

    linter

--- src/app/models/source.rb
+++ src/app/models/source.rb
@@ -6,11 +6,10 @@ class Source < ApplicationRecord
   validates :slug, presence: true, uniqueness: true
   validates :name, presence: true, uniqueness: true
 
-  
   def total_datasets_count
     completed_datasets_count + failed_datasets_count
   end
-  
+
   def completion_rate
     return 0 if total_datasets_count == 0
     (completed_datasets_count.to_f / total_datasets_count * 100).round(2)
